7de319c078204cd2c4a7e6be9cb84a7bdb1f9c00
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-06-08 21:31:57 +0300

    version 1

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 import express from "express"
 import mongoose from "mongoose"
+import multer from "multer"
 
 import {registerValidation} from "./validations/register.js"
 import {loginValidation} from "./validations/login.js";
 
-import CheakAuth from "./utils/CheakAuth.js";
+import CheckAuth from "./utils/CheckAuth.js";
 import {register, login, getMe} from "./controllers/UserController.js";
 import {create, getAll, getOne, remove, update} from "./controllers/PostController.js";
 import {postCreateValidation} from "./validations/post.js";
-import cheakAuth from "./utils/CheakAuth.js";
+import handleErrors from "./utils/handleErrors.js";
 
 // Подключение к БД
 mongoose.connect("mongodb+srv://admin:Premium14@cluster0.buwdzpn.mongodb.net/blog?retryWrites=true&w=majority")
@@ -21,7 +22,19 @@ mongoose.connect("mongodb+srv://admin:Premium14@cluster0.buwdzpn.mongodb.net/blo
 
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage})
+
 app.use(express.json())
+app.use('/uploads', express.static("uploads"))
 
 // Начальный запуск
 app.get("/", (req, res) => {
@@ -29,16 +42,24 @@ app.get("/", (req, res) => {
 })
 
 // Запросы пользователя
-app.post("/auth/login", loginValidation, login)
-app.post("/auth/register", registerValidation, register)
-app.get("/auth/me", CheakAuth, getMe)
+app.post("/auth/login", loginValidation, handleErrors, login)
+app.post("/auth/register", registerValidation, handleErrors, register)
+app.get("/auth/me", CheckAuth, getMe)
 
 //Запросы статей
-app.post("/post", cheakAuth, postCreateValidation, create)
+app.post("/post", CheckAuth, postCreateValidation, handleErrors, create)
 app.get("/posts", getAll)
 app.get("/post/:id", getOne)
-app.delete("/post/:id", cheakAuth, remove)
-app.patch("/post/:id", cheakAuth, update)
+app.delete("/post/:id", CheckAuth, remove)
+app.patch("/post/:id", CheckAuth, handleErrors, update)
+
+// Запросы Multer
+app.post("/upload", CheckAuth, upload.single('image'), (req, res) => {
+    res.json({
+        message: "Картинка загружена",
+        URL: `/uploads/${req.file.originalname}`
+    })
+})
 
 app.listen(5600,(err) => {
     if (err) {
